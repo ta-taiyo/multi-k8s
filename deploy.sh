e987b5d5bdcdea115f433511b3bda8013c7c6ec8
ta-taiyo <gakkigakki.it@gmail.com>
2019-08-12 14:34:36 +0900

    added deploy scripts7

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t ta-taiyo/multi-client:latest -t ta-taiyo/multi-client:$SHA -f ./client/Dcokerfile ./client
-docker build -t ta-taiyo/multi-server:latest -t ta-taiyo/multi-server:$SHA -f ./server/Dcokerfile ./server
-docker build -t ta-taiyo/multi-worker:latest -t ta-taiyo/multi-worker:$SHA -f ./worker/Dcokerfile ./worker
+docker build -t taiyo/multi-client:latest -t taiyo/multi-client:$SHA -f ./client/Dcokerfile ./client
+docker build -t taiyo/multi-server:latest -t taiyo/multi-server:$SHA -f ./server/Dcokerfile ./server
+docker build -t taiyo/multi-worker:latest -t taiyo/multi-worker:$SHA -f ./worker/Dcokerfile ./worker
 
-docker push ta-taiyo/multi-client:latest
-docker push ta-taiyo/multi-server:latest
-docker push ta-taiyo/multi-worker:latest
+docker push taiyo/multi-client:latest
+docker push taiyo/multi-server:latest
+docker push taiyo/multi-worker:latest
 
-docker push ta-taiyo/multi-client:$SHA
-docker push ta-taiyo/multi-server:$SHA
-docker push ta-taiyo/multi-worker:$SHA
+docker push taiyo/multi-client:$SHA
+docker push taiyo/multi-server:$SHA
+docker push taiyo/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=ta-taiyo/multi-server:$SHA
-kubectl set image deployments/client-deployment client=ta-taiyo/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=ta-taiyo/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=taiyo/multi-server:$SHA
+kubectl set image deployments/client-deployment client=taiyo/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=taiyo/multi-worker:$SHA
