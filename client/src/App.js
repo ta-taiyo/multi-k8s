408e92e7963c6b46d98b9deba65dc3a06541ffc1
ta-taiyo <gakkigakki.it@gmail.com>
2019-11-14 08:03:32 +0900

    changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,24 @@
-import './App.css';
-
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
-
-import Fib from './Fib';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className='App'>
-          <header className='App-header'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <h1 className='App-title'>Pub1,2 demo time!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
-            <Link to="/'>Home</Link'
-            <Link to='/otherpage'>Other Page</Link>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Fib Calculator UPDATED!</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
-            <Route exact path='/' component={
-      Fib} />
-            <Route path="/otherpage' component={OtherPage} /'
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
           </div>
         </div>
       </Router>
