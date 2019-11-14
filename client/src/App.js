78a897e767c2975108e406854b9cbac05afa6a63
ta-taiyo <gakkigakki.it@gmail.com>
2019-11-14 11:15:40 +0900

    pub1 pub2

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Kubernetes!!!!</h1>
+            <h1 className="App-title">pub1 pub2</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
